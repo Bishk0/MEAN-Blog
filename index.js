3cb5b399f255416184d9dcf250bc9cdc2273f36e
Bishko <yurik322412@gmail.com>
2020-10-19 17:07:12 +0300

    Routing with Express.js

--- index.js
+++ index.js
@@ -5,18 +5,29 @@ const mongoose = require('mongoose');
 const passport = require('passport');
 const path = require('path');
 const config = require('./config/db');
+const account = require('./routes/account');
 
 const app = express();
 const port = 3000;
 
-mongoose.connect(config.db, { useNewUrlParser: true, useUnifiedTopology: true});
+app.use(cors());
+app.use(bodyParser.json());
+
+mongoose.connect(config.db, {useNewUrlParser: true, useUnifiedTopology: true});
 mongoose.connection.on('connected', () => {
     console.log('Successful connection to the database');
-});mongoose.connection.on('error', (err) => {
+});
+mongoose.connection.on('error', (err) => {
     console.log('Not successful connection to the database ' + err);
 });
 
 app.listen(port, () => {
     console.log('The server was running on the port: ' + port);
 });
+
+app.get('/', (req, res) => {
+    res.send('Home page');
+});
+
+app.use('/account', account);
 
