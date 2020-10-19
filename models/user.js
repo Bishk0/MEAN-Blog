c3ccc0829fd219f60302bd79217c1103e552770c
Bishko <yurik322412@gmail.com>
2020-10-19 20:01:48 +0300

    Creating a user model

---
+++ models/user.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt')
+
+const UserSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    login: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+});
+
+const User = module.exports = mongoose.model('User', UserSchema);
+
+module.exports.getUserByLogin = function (login, cb) {
+    const query = {login: login};
+    User.findOne(query, cb);
+}
+
+module.exports.getUserById = function (id, cb) {
+    User.findById(id, cb);
+}
+
+module.exports.addUser = function (newUser, cb) {
+    bcrypt.genSalt(10, function (err, salt) {
+        bcrypt.hash(newUser.password, salt, function (err, salt) {
+            if (err) throw  err;
+            newUser.password = hash;
+            newUser.save(cb);
+        });
+    });
+}
